fb6d891a86f85851fe3d61ef594e2e90652cd469
MidasVanVeen <midasvanveen.email@gmail.com>
2021-11-27 16:14:37 +0100

    added comments

--- helloworld.s
+++ helloworld.s
@@ -1,15 +1,14 @@
-    .global _start
-    .text
+    .global _start              @ Declade global _start label. required to run.
+    .text                       @ Start of .text section
 _start:
-    MOV r7, #4
-    MOV r0, #1
-    LDR r1, =hallo
-    MOV r2, #12
-    SWI 0
+    MOV r7, #4                  @ move code for write call into r7
+    MOV r0, #1                  @ Specify fd. 1 for stdout
+    LDR r1, =hello              @ Load our 'hello' data constant into r1
+    MOV r2, #12                 @ set r2 to size of our hello message
+    SWI 0                       @ write
 
-    MOV r7, #1
-    SWI 0
+    MOV r7, #1                  @ move code for exit call into r7
+    SWI 0                       @ exit
 
-    .data
-hallo:  .ascii "hello world\n"
-
+    .data                       @ start of .data section
+hello:  .ascii "hello world\n"  @ declare hello world message
